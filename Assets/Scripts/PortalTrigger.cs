3d9a2b42eec01f6dec0651072fb768d0d858d476
LukitoGuapito <lmesquitacarmo@gmail.com>
2024-01-07 18:40:17 -0500

    portal

---
+++ Assets/Scripts/PortalTrigger.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalTrigger : MonoBehaviour
+{
+    [SerializeField] private LayerMask _playerMask;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //if()
+    }
+}
