c15a4442ab2d1391c8fe2b2bc86f86290baf59bb
FMarklew <fordmarklew123@gmail.com>
2024-01-08 20:49:36 -0500

    Upgrade system progress, some project sorting as well

--- Assets/Scripts/UI/UpgradeCard.cs
+++ Assets/Scripts/UI/UpgradeCard.cs
@@ -4,23 +4,56 @@ using TMPro;
 
 public class UpgradeCard : MonoBehaviour
 {
+    private const string animString = "UpgradeCardSpin";
+    private const string blockedStateString = "UpgradeCardBlocked";
+
     public TextMeshProUGUI titleText;
     public TextMeshProUGUI descriptionText;
 
-    public Image weaponIcon;
+    public Image abilityIcon;
     public Image gemIcon;
     public Image glowIcon;
 
+    public GameObject cardback;
+
     public RarityConfig rarityConfig;
 
-    public void Init(int tier, Sprite weaponSprite)
-	{
+    public Animator anim;
 
+    private System.Action storedCallback;
+
+    public void Init(int tier, AbilityConfig config)
+    {
+        titleText.text = config.abilityName;
+
+        abilityIcon.sprite = config.displaySprite;
+
+        glowIcon.color = rarityConfig.rarities[tier].glowColour;
+        gemIcon.color = rarityConfig.rarities[tier].gemColour;
+
+        anim.Play(blockedStateString);
+    }
+
+    public void AnimateWithCallback(System.Action callback = null)
+	{
+        storedCallback = callback;
+        anim.Play(animString);
 	}
 
+    public void AnimationDone()
+	{
+        storedCallback?.Invoke();
+	}
+
+    public AbilityConfig test_ability;
+    public int tier;
     [ContextMenu("Init")]
     public void Test()
 	{
-
+        Init(tier, test_ability);
+        AnimateWithCallback(()=>
+        {
+            Debug.Log("Animation Done");
+        });
 	}
 }
